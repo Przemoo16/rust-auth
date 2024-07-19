7e4b2c977130ddc47c0b2caa7f156e9cf9e6a15c
Przemek Kanach <kanach16@gmail.com>
2024-07-19 18:21:45 +0200

    Init templates

--- app/src/api/main.rs
+++ app/src/api/main.rs
@@ -1,15 +1,15 @@
 use crate::state::AppState;
-use axum::{extract::State, routing::get, Router};
-use sqlx::query;
+use askama_axum::Template;
+use axum::{routing::get, Router};
 
 pub fn create_main_router() -> Router<AppState> {
     Router::new().route("/", get(home))
 }
 
-async fn home(State(state): State<AppState>) -> String {
-    let res = query!("SELECT 1 as result").fetch_one(&state.db).await;
-    match res {
-        Ok(_) => "Healthy DB".to_string(),
-        Err(_) => "Unhealthy DB".to_string(),
-    }
+#[derive(Template)]
+#[template(path = "pages/home/index.html")]
+struct HomeTemplate {}
+
+async fn home() -> HomeTemplate {
+    HomeTemplate {}
 }
