435c685b980557ee2da5eec36ad7ae824c3d6c30
Przemek Kanach <kanach16@gmail.com>
2024-07-27 17:39:33 +0200

    Add protected route

--- app/src/server.rs
+++ app/src/server.rs
@@ -4,9 +4,11 @@ use crate::api::main::create_main_router;
 use crate::api::middleware::set_render_options;
 use crate::config::Config;
 use crate::db::connection::{Database, SessionStore};
+use crate::libs::auth::Backend;
 use crate::libs::signal::shutdown_signal;
 use crate::state::AppState;
-use axum::{middleware::from_fn, Router};
+use axum::{middleware::from_fn, routing::get, Router};
+use axum_login::login_required;
 use std::net::SocketAddr;
 use time::Duration;
 use tokio::{task::spawn, time::Duration as TaskDuration};
@@ -28,6 +30,11 @@ pub async fn run_server(config: ServerConfig) {
     );
     let state = AppState::new(config.db);
     let app = Router::new()
+        .route(
+            "/protected",
+            get(|| async { "Gotta be logged in to see me!" }),
+        )
+        .route_layer(login_required!(Backend, login_url = "/signin"))
         .nest("/", create_main_router())
         .nest("/", create_auth_router())
         .layer(from_fn(set_render_options))
