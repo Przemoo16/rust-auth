d6f01769f695a402ff6aba63f051b95f4e8441d9
Przemek Kanach <kanach16@gmail.com>
2024-07-27 10:32:18 +0200

    Add user retrieval

--- app/src/db/user.rs
+++ app/src/db/user.rs
@@ -1,5 +1,5 @@
 use crate::db::connection::Database;
-use sqlx::{query, Error as SqlxError};
+use sqlx::{query, query_as, Error as SqlxError};
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FormatResult};
 
@@ -47,4 +47,51 @@ pub async fn create_user(data: CreateUserData<'_>, db: &Database) -> Result<(), 
     .execute(db)
     .await?;
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct User {
+    pub id: i32,
+    pub password: String,
+}
+
+#[derive(Debug)]
+pub enum GetUserError {
+    DatabaseError(SqlxError),
+}
+
+impl Error for GetUserError {}
+
+impl From<SqlxError> for GetUserError {
+    fn from(value: SqlxError) -> Self {
+        GetUserError::DatabaseError(value)
+    }
+}
+
+impl Display for GetUserError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FormatResult {
+        match self {
+            GetUserError::DatabaseError(e) => {
+                write!(f, "Database error: {}", e)
+            }
+        }
+    }
+}
+
+pub async fn get_user_by_id(id: &i32, db: &Database) -> Result<Option<User>, GetUserError> {
+    let user = query_as!(User, "SELECT id, password FROM users WHERE id = $1", id)
+        .fetch_optional(db)
+        .await?;
+    Ok(user)
+}
+
+pub async fn get_user_by_email(email: &str, db: &Database) -> Result<Option<User>, GetUserError> {
+    let user = query_as!(
+        User,
+        "SELECT id, password FROM users WHERE email = $1",
+        email
+    )
+    .fetch_optional(db)
+    .await?;
+    Ok(user)
 }
