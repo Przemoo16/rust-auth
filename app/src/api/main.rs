465c0d686eaa2457b1447db68348600a437311ef
Przemek Kanach <kanach16@gmail.com>
2024-07-28 12:03:01 +0200

    Add detect login logic

--- app/src/api/main.rs
+++ app/src/api/main.rs
@@ -1,5 +1,5 @@
-use crate::api::middleware::RenderOptions;
 use crate::state::AppState;
+use crate::{api::middleware::RenderOptions, libs::auth::AuthSession};
 use askama_axum::Template;
 use axum::{extract::Extension, routing::get, Router};
 
@@ -11,8 +11,16 @@ pub fn create_main_router() -> Router<AppState> {
 #[template(path = "home/index.html")]
 struct HomeTemplate {
     options: RenderOptions,
+    is_authenticated: bool,
 }
 
-async fn home(Extension(options): Extension<RenderOptions>) -> HomeTemplate {
-    HomeTemplate { options }
+async fn home(
+    Extension(options): Extension<RenderOptions>,
+    auth_session: AuthSession,
+) -> HomeTemplate {
+    let is_authenticated = auth_session.user.is_some();
+    HomeTemplate {
+        options,
+        is_authenticated,
+    }
 }
